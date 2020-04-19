a11b2a4a9a11dee8507ba096f78a6acebe31b2ad
Michael <44589253+mme384@users.noreply.github.com>
2020-04-19 21:05:30 +0200

    Original upload

---
+++ train.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+    FILE NAME: train.py
+    AUTHOR: Michalis Meyer
+    DATE CREATED: 
+    DATE LAST MODIFIED: 
+    PYTHON VERSION: 
+    SAMPLE COMMAND LINE: 
+    SCRIPT PURPOSE: Train neural network.
+"""
