24e5bfc886df71b8a1ff4a3bc957f95f8698ac3b
Michael <44589253+mme384@users.noreply.github.com>
2020-04-19 21:05:08 +0200

    Original upload

---
+++ predict.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+    FILE NAME: predict.py
+    AUTHOR: Michalis Meyer
+    DATE CREATED: 
+    DATE LAST MODIFIED: 
+    PYTHON VERSION: 
+    SCRIPT PURPOSE: 
+"""
